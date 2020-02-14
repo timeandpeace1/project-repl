c00223f7b9effbc587a537b210dc9cbae583c087
timeandpeace1 <munchinc.00@gmail.com>
2020-02-14 02:34:22 +0000

    added comments and changed opening question

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("enter distance in rods")
+  // you first enter your value in kelvins
+  fmt.Println("enter kelvin")
   var first float32
   fmt.Scanln(&first)
-
-  fmt.Println(first-273.15)
-  fmt.Println((first-273.15)*(9.0/5)+32)
+  // your value is then converted to celcius then farenheit
+  fmt.Println("your value in celcius is", first-273.15)
+  fmt.Println("your value in farenheit is", (first-273.15)*(9.0/5)+32)
 }
